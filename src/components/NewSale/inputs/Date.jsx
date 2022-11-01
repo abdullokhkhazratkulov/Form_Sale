3233b731e714a243ca09a9bec6135c98ef968819
Abdullokh <abdullokhkhazratkulov@gmail.com>
2022-11-02 02:59:32 +0500

    add date

---
+++ src/components/NewSale/inputs/Date.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Moment from 'moment'
+
+const Date = () => {
+  return (
+    <div className="currentDate">
+        <input type="date" min="02-11-2022"/>
+    </div>
+  )
+}
+
+export default Date
